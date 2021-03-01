25be7c0b303e1024a090261a9675446677a53fc6
Dileep <dileep.srimani@gmail.com>
2021-03-01 22:47:43 +0530

    add car model and keep track of number of cars made

--- src/Application.java
+++ src/Application.java
@@ -3,21 +3,20 @@ public class Application {
 
     public static void main(String[] args) {
         Car blueCar = new Car("Blue");
-        System.out.println(blueCar.getColor());
-        blueCar.getEngine().start();
-        blueCar.getEngine().printStatusOfEngine();
+        blueCar.printModelAndNumberOfCarsMade();
+
+//        look at the implementation of this method for error
+//        blueCar.printStatusOfEngine();
 
         System.out.println("-------------------------------");
 
         Car redCar = new Car("Red");
-        System.out.println(redCar.getColor());
-        redCar.getEngine().stop();
-        redCar.getEngine().start();
-        redCar.getEngine().printStatusOfEngine();
-        redCar.getEngine().start();
-        redCar.getEngine().stop();
-        redCar.getEngine().stop();
-        redCar.getEngine().printStatusOfEngine();
+        redCar.printModelAndNumberOfCarsMade();
+
+        System.out.println("-------------------------------");
+
+        Car greenCar = new Car("Green");
+        greenCar.printModelAndNumberOfCarsMade();
     }
 
 }
--- src/Car.java
+++ src/Car.java
@@ -1,16 +1,22 @@
 public class Car {
 
+    private static String model = "Ford Fusion Energi";
+    private static int numberOfCarsMade;
     private String color;
     private Engine engine = new Engine();
 
-    public Car(){}
+    public Car(){
+        numberOfCarsMade = numberOfCarsMade + 1;
+    }
 
     public Car(String color){
         this.color = color;
+        numberOfCarsMade = numberOfCarsMade + 1;
     }
 
     public Car(Car car){
         this.color = car.color;
+        numberOfCarsMade = numberOfCarsMade + 1;
     }
 
     public String getColor() {
@@ -23,5 +29,23 @@ public class Car {
 
     public Engine getEngine() {
         return engine;
+    }
+
+    public void printModelAndNumberOfCarsMade(){
+        System.out.println("model = " + model);  // from  a non static method can access static property
+        System.out.println("number Of Cars Made = " + getNumberOfCarsMade());  // from a non static method can access static method
+    }
+
+//    ERROR: Non-static field 'engine' cannot be referenced from a static context
+//    public static void printStatusOfEngine(){
+//        engine.printStatusOfEngine();
+//    }
+
+    public static String getCarModel(){
+        return model;
+    }
+
+    public static int getNumberOfCarsMade(){
+        return numberOfCarsMade;
     }
 }
