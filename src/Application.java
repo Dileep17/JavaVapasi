9e6be58f1e76e8e0fd5a3c391fb231867b00c2f0
Dileep <dileep.srimani@gmail.com>
2021-03-01 23:27:07 +0530

    add cars hashmap

--- src/Application.java
+++ src/Application.java
@@ -1,31 +1,36 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Application {
 
     public static void main(String[] args) {
-        List<Car> cars = new ArrayList<>();
+        Map<String, Car> cars = new HashMap<>();
 
         Car blueCar = new Car("Blue");
-        cars.add(blueCar);
+        cars.put("Blue", blueCar);
 
         Car redCar = new Car("Red");
-        cars.add(redCar);
+        cars.put("Red", blueCar);
 
         Car GreenCar = new Car("Green");
-        cars.add(GreenCar);
+        cars.put("Green", blueCar);
 
-        Iterator<Car> carIterator = cars.iterator();
-        while (carIterator.hasNext()){
-            System.out.println(carIterator.next().getColor());
+
+        System.out.println("Keys in HashMap: ");
+        for (String key : cars.keySet()){
+            System.out.println(key);
         }
 
-        // or
-
-        for (Car car : cars){
-            System.out.println(car.getColor());
+        System.out.println("Enter one of the keys to view the details of car");
+        Scanner scanner = new Scanner(System.in);
+        String key = scanner.next();
+        if(cars.containsKey(key)){
+            Car car = cars.get(key);
+            System.out.println("color = " + car.getColor());
+            car.getEngine().printStatusOfEngine();
+        } else {
+            System.out.println("Key doesn't exist! Remember key is case sensitive.");
         }
+
 
     }
 
